3427a3084177d1b1e348d467192e700a4c8f4557
Firefly4266 <gaines.johnp@gmail.com>
2017-09-15 15:52:57 -0700

    Not complete

---
+++ Properties/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    class Employee
+    {
+        private DateTime hireDate;
+        private string employeeName;
+
+        public string EmployeeName
+        {
+            get
+            {
+                return employeeName;
+            }
+            set
+            {
+                this.employeeName = value;
+            }
+        }
+        public DateTime HireDate
+        {
+            get
+            {
+                return hireDate.;
+            }
+            set
+            {
+                hireDate = value;
+            }
+        }
+    }
+}
--- Properties/Program.cs
+++ Properties/Program.cs
@@ -10,6 +10,18 @@ namespace Properties
     {
         static void Main(string[] args)
         {
+            Employee employee = new Employee();
+
+            Console.Write("\n Greetings user, please enter an employee name: ");
+            string newEmployee = Console.ReadLine();
+
+            employee.EmployeeName = newEmployee;
+
+            Console.Write("\n Please enter a start date: ");
+            employee.HireDate = DateTime.  Parse(Console.ReadLine());
+
+            Console.WriteLine($"\n Employee Name: {employee.EmployeeName}");
+            Console.WriteLine($"\n Employee Start Date: {employee.HireDate}\n");
         }
     }
 }
